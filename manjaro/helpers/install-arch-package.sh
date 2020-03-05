651f18742334d9f5ca2a22d9e503d45de950019e
Petherson Moreno <pethermoreno@gmail.com>
2020-03-05 03:19:13 -0300

    fix: fixed install arch linux package when exist original package before makepkg

--- manjaro/helpers/install-arch-package.sh
+++ manjaro/helpers/install-arch-package.sh
@@ -5,7 +5,8 @@ curl "https://aur.archlinux.org/cgit/aur.git/snapshot/${PACKAGE_NAME}.tar.gz" --
 tar -zxvf "${PACKAGE_NAME}.tar.gz"
 cd "${PACKAGE_NAME}"
 makepkg -s
-sudo pacman -U *.xz
+PACKAGE_INSTALL=`ls | grep .tar.xz | grep -v orig`
+sudo pacman -U $PACKAGE_INSTALL
 cd ..
 rm -rf "${PACKAGE_NAME}"
 rm "${PACKAGE_NAME}.tar.gz"
