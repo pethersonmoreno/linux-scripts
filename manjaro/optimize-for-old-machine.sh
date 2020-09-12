a1a202181346f658b07422008204d0bfc70082a4
Petherson Moreno <pethermoreno@gmail.com>
2020-09-12 14:09:22 -0300

    Changed to tuning in old machine

--- manjaro/optimize-for-old-machine.sh
+++ manjaro/optimize-for-old-machine.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 sudo tee -a /etc/sysctl.d/99-sysctl.conf <<-EOF
-vm.swappiness=10
-vm.vfs_cache_pressure=50
-vm.dirty_background_bytes=16777216
-vm.dirty_bytes=50331648
+vm.min_free_kbytes=262144
+vm.swappiness=1
+vm.vfs_cache_pressure=80
+vm.dirty_background_ratio=5
+vm.dirty_ratio=10
 EOF
+
+#default
+# vm.min_free_kbytes=67584
+# vm.swappiness=60
+# vm.vfs_cache_pressure=100
+# vm.dirty_background_bytes = 0
+# vm.dirty_background_ratio = 20
+# vm.dirty_bytes = 0
+# vm.dirty_ratio = 50
