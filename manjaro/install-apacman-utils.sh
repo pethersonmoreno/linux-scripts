61081e1bf40c9383e1fbfc586ab0c68a9a3ba999
Petherson Moreno <pethermoreno@gmail.com>
2020-10-10 17:07:29 -0300

    feat: added intallers to apacman-deps and apacman-utils

---
+++ manjaro/install-apacman-utils.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+./helpers/install-arch-package.sh pkgbuild-introspection
+PACKAGE_NAME=apacman-utils
+TMP_DIRECTORY=/tmp
+if [ $# -gt 1 ]; then
+  TMP_DIRECTORY=$2
+fi
+if [ ! -d "$TMP_DIRECTORY" ]; then
+  echo "Temp Directory \"$TMP_DIRECTORY\" DOES NOT exists!"
+  exit 1
+fi
+cd $TMP_DIRECTORY
+curl "https://aur.archlinux.org/cgit/aur.git/snapshot/${PACKAGE_NAME}.tar.gz" --output "${PACKAGE_NAME}.tar.gz"
+tar -zxvf "${PACKAGE_NAME}.tar.gz"
+cd "${PACKAGE_NAME}"
+sed -i '/^makedepends/d' PKGBUILD
+makepkg -s
+PACKAGE_INSTALL=`ls | grep .tar.xz | grep -v orig`
+sudo pacman -U $PACKAGE_INSTALL
+cd ..
+rm -rf "${PACKAGE_NAME}"
+rm "${PACKAGE_NAME}.tar.gz"
