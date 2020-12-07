ae6b28d72feb0d95975d1146d46421a2d0a9e4f5
Petherson Moreno <pethermoreno@gmail.com>
2020-12-06 21:13:27 -0300

    feat: updated install arch package script to consider generated .tar.zst generated file

--- manjaro/helpers/install-arch-package.sh
+++ manjaro/helpers/install-arch-package.sh
@@ -13,7 +13,7 @@ curl "https://aur.archlinux.org/cgit/aur.git/snapshot/${PACKAGE_NAME}.tar.gz" --
 tar -zxvf "${PACKAGE_NAME}.tar.gz"
 cd "${PACKAGE_NAME}"
 makepkg -s
-PACKAGE_INSTALL=`ls | grep .tar.xz | grep -v orig`
+PACKAGE_INSTALL=`ls | grep -E '\.tar\.xz|\.tar\.zst' | grep -v orig`
 sudo pacman -U $PACKAGE_INSTALL
 cd ..
 rm -rf "${PACKAGE_NAME}"
