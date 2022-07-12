6d3d784923202b93c341a79e26327dc02affbd47
Petherson Moreno <pethermoreno@gmail.com>
2022-07-12 18:20:13 -0300

    feat: added installers of packer

---
+++ others/install-packer-386.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Finding last version of packer"
+VERSION=`curl -s https://api.github.com/repos/hashicorp/packer/releases/latest | grep tag_name | sed -e 's/^\s*"tag_name"\s*:\s*"v\?\([^"]\+\).\+/\1/'`
+CURRENT_VERSION=`type packer >/dev/null 2>&1 && packer --version`
+if [ "$CURRENT_VERSION" = "$VERSION" ]; then
+  echo "Your packer version is already latest ${VERSION}"
+  exit 0
+fi
+if [ ! -z "$CURRENT_VERSION" ]; then
+  echo "Your packer current version is ${CURRENT_VERSION}"
+fi
+echo "packer last version is ${VERSION}"
+ARCHTECTURE=386
+FILE_ZIP="packer_${VERSION}_linux_${ARCHTECTURE}.zip"
+curl -L "https://releases.hashicorp.com/packer/${VERSION}/${FILE_ZIP}" > "/tmp/${FILE_ZIP}"
+unzip "/tmp/${FILE_ZIP}" -d /tmp
+sudo mv /tmp/packer /usr/local/bin/
+rm "/tmp/${FILE_ZIP}"
